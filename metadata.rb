4c17406c642ee333b2c003d48920e45b88b1e783
Eric Klusman <eklusman@kabaminc.com>
2017-05-24 12:58:21 -0700

    removed "issues_url", "source_url", which RightScale did not like

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,4 @@ license          'Apache 2.0'
 description      'Provides a way to create a visual marker in the Chef log based on a template.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
-issues_url       'https://github.com/rightscale-cookbooks/marker/issues'
-source_url       'https://github.com/rightscale-cookbooks/marker'
 chef_version '>= 12.0' if respond_to?(:chef_version)
